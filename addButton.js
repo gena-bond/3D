1e4d780de91c440a81ef10853a1891c137f73143
Gennady <gena.bondarenko@i.ua>
2018-12-26 18:15:38 +0300

    2018-12-26 Send bbox

--- addButton.js
+++ addButton.js
@@ -1,42 +1,58 @@
-//var frameSrc = mediaLink +'&path=/media/js/Trento_Catasto/index.html';
-lizMap.addDock('threejs', '3D', 'right-dock', 'http://localhost/Apps/index.html', 'icon-globe');
-//alert('ura');
+var frameSrc = 'about:blank';
+//add newDocument 
+lizMap.addDock(
+    'threejs', 
+    '3D', 
+    'right-dock', 
+    '',
+    //'<iframe id="receiver" src="' + frameSrc + '" height="800px" width="100%">', 
+    'icon-globe');
 
 lizMap.events.on({
     'uicreated':function(evt){
          //Start//
-         var newHomeLink = 'http://localhost/Apps/index.html';
+         var newHomeLink = 'http://www.cesium.mysite.org/Apps/index.html';
         // Replace home icon URL
-   
         $('#button-threejs').click(function(){
-        	$('#button-permaLink').click();
-
-        	coord = $('#input-share-permalink').val();
-			var getLocation = function(href) {
+            //Read bbox-from 'button'-permaLink
+            var permaLink_window = $('#button-permaLink').click();
+            //String link of bbox coordinates
+        	bbox_link_string = $('#input-share-permalink').val();             
+            
+            //Function search a href element
+            var getLocation = function(href) {
     		var l = document.createElement("a");
     			l.href = href;
     			return l;
 			};
-			var l = getLocation(coord);
-			//console.log(document); 
-			//console.log('host: '+l.hostname);
-			//console.log('search: '+l.search);
-			bbox_coord = getQueryVariable(l.search,'bbox');
-        	console.log('bbox: ' + bbox_coord);
-			//window.open(newHomeLink, '3D', '');
-			var newWin = window.open(newHomeLink, '3D', '');
-			//alert(newWin.location.href);
+            
+            //Выделяет только 
+			var bbox_link = getLocation(bbox_link_string);
+            			
+            //Отделяет bbox от остальной ссылки
+			bbox_coord = getQueryVariable(bbox_link.search,'bbox');
+        	
+            //Записывет cookie с bbox
+            var cookieName = 'bbox';
+            var cookieValue = bbox_coord;
+            document.cookie = cookieName + "=" + cookieValue + ";domain=.mysite.org;path=/";
+            //console.log('cookie = ' + document.cookie);
+            
+            //Открывает новое окно 
+			var newWindow = window.open(newHomeLink, '3D', '');
 
-			newWin.onload = function() {
-				localStorage.setItem("storageName",bbox_coord);
-			}
+            /*Close #button-permaLink
+            newWindow.onload(function(){
+                 permaLink_window.close();
+            });*/
+		
   		return false;
 		})
      }
 });
 
 function getQueryVariable(query,variable) {
-    //var query = window.location.search.substring(1);
+    
     var vars = query.split('&');
     for (var i = 0; i < vars.length; i++) {
         var pair = vars[i].split('=');
@@ -46,20 +62,3 @@ function getQueryVariable(query,variable) {
     }
     console.log('Query variable %s not found', variable);
 };
-
-/*lizMap.events.on({
-uicreated: function(e) {
-var mediaLink = OpenLayers.Util.urlAppend(
-lizUrls.media,
-OpenLayers.Util.getParameterString(lizUrls.params)
-);
-var frameSrc = mediaLink +'&path=/media/js/mapnik/Cesium/Apps/index.html';
-lizMap.addDock(
-'threejs',
-'Vue 3D',
-'right-dock',
-'<iframe src="' + frameSrc + '" height="800px" width="100%">',
-'icon-globe'
-);
-}
-});*/
